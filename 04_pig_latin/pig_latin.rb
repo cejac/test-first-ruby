720c376aa90a1ddedb77efa2254292f9e0fca41e
Carolina <cejac10@gmail.com>
2017-09-27 20:41:13 +0000

    all tests passing

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,19 +1,30 @@
 #write your code here
-def translate(word)
-  letters = ('a'...'z').to_a 
-  vowels = %w[a e i o u]
-  cons = letters - vowels
-
-  if vowels.include?(word[0])
-   word + 'ay'
-  elsif cons.include?(word[0]) && cons.include?(word[1])
-   word[2..-1] + word[0..1] + 'ay'
-  elsif cons.include?(word[0])
-    word[1..-1] + word[0] + 'ay'
-
-    
+def translate phrase
+ split = phrase.split(' ')
+ complete = []
+ alpha = ('a'..'z').to_a
+ vowels = %w[a e i o u]
+ cons = alpha - vowels
+ 
+ split.each do |x|
+  if vowels.include?(x[0])
+   complete.push(x + 'ay')
+  elsif x[0] == 'q' && x[1] == 'u'
+   complete.push(x[2..-1] + x[0..1] + 'ay')
+  elsif x[1] == 'q' && x[2] == 'u'
+   complete.push(x[3..-1] + x[0..2] + 'ay')
+  elsif cons.include?(x[0]) && cons.include?(x[1]) && cons.include?(x[2])
+   complete.push(x[3..-1] + x[0..2] + 'ay')
+  elsif cons.include?(x[0]) && cons.include?(x[1])
+   complete.push(x[2..-1] + x[0..1] + 'ay')
+  elsif cons.include?(x[0])
+   complete.push(x[1..-1] + x[0] + 'ay')
   end
+ 
   
+ end
+ complete.join(' ')
+ 
 
 end
 
