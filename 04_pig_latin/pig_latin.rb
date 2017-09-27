7c8f9a53a1973014c68e546b3806e01909b76127
Carolina <cejac10@gmail.com>
2017-09-27 17:35:03 +0000

    first test passing

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,13 @@
 #write your code here
+def translate(word)
+  letters = ('a'...'z').to_a 
+  vowels = %w[a e i o u]
+  cons = letters - vowels
+
+  if vowels.include?(word[0])
+   word + 'ay'
+  end
+  
+
+end
+
