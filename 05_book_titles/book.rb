197d237cde7e0e667407cec43641265f30eb0ad2
Carolina <cejac10@gmail.com>
2017-10-17 23:24:43 +0000

    took a while, but first test passing

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,47 @@
 class Book
 # write your code here
+  attr_accessor :title
+  
+  def title
+      titleize(@title)
+  end
+  
+  def titleize(bookTitle)
+    words = bookTitle.split(" ")
+    words[0].capitalize!
+  end
+  
 end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+#    def title
+#     titleize( @title)
+#    end
+#
+#    def titleize(sentence)
+#      words = sentence.split
+#      little_words = %w{and or over the in of a an}
+#      words.each_with_index do |word , i|
+#        if(little_words.include?(word))
+#          modified_word = i == 0 ? word.capitalize : word;
+#        else
+#          modified_word = word.capitalize
+#        end
+#        words[i] = modified_word;
+#      end
+#      words.join " "
+#    end
+#end
