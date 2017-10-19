88ba58f6a40bdeb6487f6bbc9cb395153007cf2d
Carolina <cejac10@gmail.com>
2017-10-19 17:42:28 +0000

    all tests passing

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -7,9 +7,18 @@ class Book
   end
   
   def titleize(bookTitle)
+    excluded = %w[the a an and in of too]
     words = bookTitle.split(" ")
-    words.each {|word| word.capitalize! unless word === 'and'}
+    words.each {|word| 
+      if excluded.include?(word)
+        word.downcase
+      else
+        word.capitalize!
+      end
+    }
+    words[0].capitalize!
     words.join(" ")
+    
   end
   
 end
